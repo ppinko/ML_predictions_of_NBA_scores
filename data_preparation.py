3f17b2e3df4eedc119a744ea2b96cb7e5ff51658
ppinko <p.pinkowicz@gmail.com>
2022-04-04 19:27:46 +0200

    IMPLEMENT - Extract data for each team

--- data_preparation.py
+++ data_preparation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
+import csv
 
 
 def transform_to_datatime(date):
@@ -12,6 +13,69 @@ def transform_to_datatime(date):
         date[1] = '0' + date[1]
     date = ' '.join(date)
     return datetime.strptime(date, '%B %d %Y')
+
+
+def create_team_data(df, team, season):
+    '''
+    Store csv data for a particular team for particular season.
+    '''
+    vals = {'no.': 0, 'valid': 0, 'date': None, 'home': 0, 'last_game_win': 0, 'tot_win': 0,
+            'tot_home_game': 0, 'last_home': 0, 'last_home_win': 0,
+            'last_away_win': 0, 'last_10': 0, 'last_5': 0, 'last_5_home': 0,
+            'last_5_away': 0}
+
+    prev_game = {'home': False, 'win': False}
+
+    df_team = df[(df['HomeTeam'] == team) | (df['AwayTeam'] == team)]
+
+    out_file = team + '_' + season + '.csv'
+    with open(out_file, 'w', newline='') as csvfile:
+        csvWriter = csv.writer(csvfile, delimiter=',')
+        csvWriter.writerow(vals.keys())
+        for _, row in df_team.iterrows():
+            prev_game['home'] = row['HomeTeam'] == team
+            prev_game['win'] = row['WinningTeam'] == team
+
+            # update basic vals
+            vals['no.'] = vals['no.'] + 1
+            vals['data'] = row['Date']
+            vals['home'] = int(row['HomeTeam'] == team)
+
+            # SAVE ROW
+            csvWriter.writerow(vals.values())
+
+            # update rest of the values for next iterations
+            if prev_game['win']:
+                vals['tot_win'] = vals['tot_win'] + 1
+                vals['last_game_win'] = 1
+                vals['last_5'] = ((vals['last_5'] << 1) | 1) % 32
+                vals['last_10'] = ((vals['last_10'] << 1) | 1) % 2**10
+            else:
+                vals['last_game_win'] = 0
+                vals['last_5'] = ((vals['last_5'] << 1) | 0) % 32
+                vals['last_10'] = ((vals['last_10'] << 1) | 0) % 2**10
+
+            if prev_game['home']:
+                vals['tot_home_game'] = vals['tot_home_game'] + 1
+                vals['last_home'] = 1
+                if prev_game['win']:
+                    vals['last_home_win'] = 1
+                    vals['last_5_home'] = ((vals['last_5_home'] << 1) | 1) % 32
+                else:
+                    vals['last_home_win'] = 0
+                    vals['last_5_home'] = ((vals['last_5_home'] << 1) | 0) % 32
+            else:
+                vals['last_home'] = 0
+                if prev_game['win']:
+                    vals['last_away_win'] = 1
+                    vals['last_5_away'] = ((vals['last_5_away'] << 1) | 1) % 32
+                else:
+                    vals['last_away_win'] = 0
+                    vals['last_5_away'] = ((vals['last_5_away'] << 1) | 0) % 32
+
+            # enough data for making predictions
+            if vals['valid'] or (vals['no.'] - vals['tot_home_game'] >= 5 and vals['tot_home_game'] >= 5):
+                vals['valid'] = 1
 
 
 file_name = '2019-20_pbp.csv'
@@ -26,9 +90,10 @@ df = df[(df['AwayPlay'] == eog) & (df['GameType'] == gameType)]
 df['HomeWin'] = np.uint8(df.WinningTeam == df.HomeTeam)
 df['date'] = df.Date.apply(transform_to_datatime)
 
-print(df.columns)
-
-print()
-print(df.head)
-
-print()
+# create input files
+df2 = df[['HomeTeam']]
+df2.drop_duplicates(inplace=True)
+df2.sort_values('HomeTeam', inplace=True)
+for i in range(df2.shape[0]):
+    team = str(df2.iat[i, 0])
+    create_team_data(df, team, '2019-20')
